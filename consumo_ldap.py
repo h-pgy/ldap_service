f798d1f36557ffc719f465c8f20619e4a46dafbd
h-pgy <hpougy@gmail.com>
2020-03-11 11:32:08 -0300

    criei funcao padrao para consumir ldap

---
+++ consumo_ldap.py
@@ -0,0 +1,15 @@
+import requests
+
+def validar_ad(user, passw, ip_ldap, porta_ldap):
+
+    url = f'https://{ip_ldap}:{porta_ldap}/conexao_ldap'
+
+    s = requests.Session()
+    s.trust_env = False #garante que roda na intranet
+
+    with s.post(url, data = {'user' : user, 'passw' : passw}) as r:
+
+        resposta = r.json()
+        assert resposta['request_status'] == 'success'
+
+    return resposta['response']
